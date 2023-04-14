59333e1735daf97b3ebb7739ed0eb881b85f2871
maayanramot <maayanramot1@gmail.com>
2023-04-14 09:10:35 +0300

    commit before socket.io

--- src/models/CodeBlockModel.ts
+++ src/models/CodeBlockModel.ts
@@ -2,11 +2,13 @@ import { Schema } from "mongoose";
 import mongoose from "mongoose"
 
 export interface ICodeBlockModel {
+    _id: string
     title: string
     content: string
 }
 
 export const codeBlockSchema = new Schema<ICodeBlockModel>({
+    _id: { type: String, required: true },
     title: { type: String, required: true },
     content: { type: String, required: true }
 })
