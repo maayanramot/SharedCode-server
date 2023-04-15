90279eac2bc84993a43479dd8bd8f8b7e17f57ae
maayanramot <maayanramot1@gmail.com>
2023-04-16 01:56:18 +0300

    change model

--- src/models/CodeBlockModel.ts
+++ src/models/CodeBlockModel.ts
@@ -4,13 +4,15 @@ import mongoose from "mongoose"
 export interface ICodeBlockModel {
     _id: string
     title: string
-    content: string
+    code: string
+    missingCode: string
 }
 
 export const codeBlockSchema = new Schema<ICodeBlockModel>({
     _id: { type: String, required: true },
     title: { type: String, required: true },
-    content: { type: String, required: true }
+    code: { type: String, required: true },
+    missingCode: { type: String, required: true }
 })
 
 export const CodeBlockModel = mongoose.model<ICodeBlockModel>('codeblocks', codeBlockSchema)
